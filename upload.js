28ea1c47ebc8bd9c276229a5b3705f245b20b774
Alex Bakhtin <Alex.Bakhtin@gmail.com>
2017-09-30 07:44:40 +0300

    Добавлен первый вариант.

---
+++ upload.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const program = require('commander');
+
+let YandexDisk = require('yandex-disk').YandexDisk;
+
+program
+    .option('-s, --size <size>', 'Chunk size')
+    .option('-f, --file <path>', 'Source file')
+    .option('-d, --destination <path>', 'Yandex.Disk destination path')
+    .option('-u, --username <user>', 'Yandex.Disk user name')
+    .option('-p, --password <password>', 'Yandex.Disk password')
+    .parse(process.argv);
+
+
+
+try {
+
+    let errors = [];
+    
+    let fileName = program.file;
+    if (!fileName) errors.push( new Error('<file> required') );
+    
+    let dstPath = program.destination;
+    if (!dstPath) errors.push( new Error('<destination> required') );
+    
+    let chunkSize = Number(program.size);
+    if (!chunkSize) errors.push( new Error('<size> required') );
+    
+    let user = program.username;
+    if (!user) errors.push( new Error('<username> required') );
+    
+    let password = program.password;
+    if (!password) errors.push( new Error('<password> required') );
+
+    if ( errors.length > 0 ) throw errors;
+    let disk = new YandexDisk(user, password);
+    
+    disk.mkdir( dstPath, function(err) {
+	if (err) {
+	    console.log(err);
+	}
+	else {
+	    disk.uploadSplitFile(fileName, dstPath, chunkSize, function(err, res) {
+	    });
+	};
+    });
+}
+catch (errors) {
+    errors.forEach(function(error, i, arr) {
+	console.log("" + error);
+    });
+    program.outputHelp();
+};
